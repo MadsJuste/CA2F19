0582027a127bd75d80bd047ca1dccface9af379e
MadsJuste <Madsjuste89@gmail.com>
2019-03-12 12:26:40 +0100

    getters for rest

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(rest.PersonResource.class);
+        resources.add(rest.ZipResource.class);
     }
     
 }
--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -5,14 +5,20 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -21,9 +27,11 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("person")
 public class PersonResource {
-
+    
     @Context
     private UriInfo context;
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    Facade f = new Facade();
 
     /**
      * Creates a new instance of PersonResource
@@ -35,19 +43,73 @@ public class PersonResource {
      * Retrieves representation of an instance of rest.PersonResource
      * @return an instance of java.lang.String
      */
+    
     @GET
+    @Path("{street}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public Response getPersonsByAddress(@PathParam("street") String steet) {
+       Address address = gson.fromJson(street, Address.class);
+       return Response.ok().entity(gson.toJson(f.getPersonsByAddress(address))).build();
     }
+    
+    @GET
+    @Path("{phone}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPersonByPhone(@PathParam("phone") String phone) {
+        Phone p = gson.fromJson(phone, Phone.class);
+       return Response.ok().entity(gson.toJson(f.getPersonByPhone(p))).build();
+    }
+    
+    @GET
+    @Path("{hobby}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPersonsByHobby(@PathParam("hobby") String hobby) {
+        Hobby h = gson.fromJson(hobby, Hobby.class);
+      return Response.ok().entity(gson.toJson(f.getPersonsByHobby(h))).build();
+    }
+    
+    @GET
+    @Path("{zip}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPersonsByZip(@PathParam("zip") String zip) {
+         Cityinfo ci = gson.fromJson(zip, Cityinfo.class);
+      return Response.ok().entity(gson.toJson(f.getPersonsByZip(ci))).build();
+    }
+    
+    @GET
+    @Path("count/{hobby}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCountByHobby(@PathParam("hobby") String hobby) {
+         Hobby h = gson.fromJson(hobby, Hobby.class);
+      return Response.ok().entity(gson.toJson(f.getCountByHobby(h))).build();
+    }
+    
+    
 
     /**
      * PUT method for updating or creating an instance of PersonResource
      * @param content representation for the resource
      */
     @PUT
+    @Path("{phone}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public void editPersonByPhone(String phone) {
+        
+        
+    }
+    
+    @DELETE
+    @Path("delete/{phone}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deletePersonByPhone(@PathParam("phone") String phone) {
+        Phone p = gson.fromJson(phone, Phone.class);
+        return Response.ok().entity(gson.toJson(f.deletePerson(phone))).build();
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addPerson(String content) {
+        pf.addPerson(JSONConverter.getPersonFromJson(content));
+        return Response.ok().build();
     }
 }
