a325dfc4123b29c34cae89aeb9dea1c6f9eda87e
Mark <mark.d.denner@gmail.com>
2019-03-13 11:15:47 +0100

    made entity Hobby column -name- unique

--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -3,6 +3,7 @@ package entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Hobby implements Serializable {
     
     private String description;
     
+    @Column(unique = true)
     private String name;
 
     public Hobby() {
