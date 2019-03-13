c131cda9a250980b6db65153f5fb3537b95b21b4
jenzuffer <35786745+jenzuffer@users.noreply.github.com>
2019-03-13 10:37:27 +0100

    adding dto

--- src/main/java/dto/AllCitiesAndAllZipCodesDTO.java
+++ src/main/java/dto/AllCitiesAndAllZipCodesDTO.java
@@ -5,10 +5,32 @@
  */
 package dto;
 
+import entities.CityInfo;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentMap;
+import org.eclipse.persistence.internal.helper.ConcurrencyManager;
+
 /**
  *
  * @author Christian
  */
 public class AllCitiesAndAllZipCodesDTO {
-    
+
+    private LinkedHashMap<String, String> cityzipMap = new LinkedHashMap();
+
+    public AllCitiesAndAllZipCodesDTO(List<CityInfo> cityinfoList) {
+        for (CityInfo cityinfo : cityinfoList) {
+            cityzipMap.put(cityinfo.getCity(), cityinfo.getZip());
+        }
+    }
+
+    public LinkedHashMap<String, String> getCityzipMap() {
+        return cityzipMap;
+    }
+
+    public void setCityzipMap(LinkedHashMap<String, String> cityzipMap) {
+        this.cityzipMap = cityzipMap;
+    }
+
 }
