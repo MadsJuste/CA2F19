7009ba344438ecbcf015eb51c6f99443d897f92f
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro.local>
2019-03-15 13:33:56 +0100

    sql

---
+++ src/main/java/utils/SQLGenerator.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.util.Random;
+
+/**
+ *
+ * @author mwn
+ */
+public class SQLGenerator {
+
+    private static String[] addresses = {"Enghaven", "Mølleager", "Gartnerhaven", "Birkevej", "Hesselhaven", "Solbakken", "Sorøvej", "Ved Fjorden", "Grendalsvej", "Næstvedvej", "Enghaven", "Solbakken", "Grendalsvej", "Gl. Strand", "Frederikskaj", "Overgade", "Vermlandsgade", "Nyropsgade", "Blegdamsvej", "Smakkedalen", "John Tranums Vej", "Kampmannsgade", "Snorresgade", "Molestien", "Weidekampsgade", "Klokhøjen", "Ramsingsvej", "Brolæggerstræde"};
+    private static String[] firstNames = {"Ida", "Ella", "Emma", "Alma", "Sofia", "Keld", "Kell", "Kelvin", "Kenn", "Kenneth", "Kenni", "Nicklas", "Nicky", "Nicolas", "Niels", "Tobias", "Tristan", "Camilla", "Carin", "Carina", "Carla", "Carmen", "Carola", "Carolina", "Caroline", "Jane", "Jani", "Janice", "Janni", "Jannie", "Jasmin", "Jeane", "Jeanet", "Oda", "Odine", "Ofelia", "Silje", "Silke", "Simone", "Sissel", "Siv", "Smilla", "Soffie", "Sofia", "Sofie"};
+    private static String[] lastNames = {"Andreassen", "Bisgård", "Bisgaard", "Bjerre", "Bjerregård", "Bjerregaard", "Bonde", "Brandt", "Brodersen", "Bruun", "Buch", "Bundgård", "Bundgaard", "Carlsen", "Carstensen", "Friis", "Frost", "Gade", "Gregersen", "Hald", "Hansen", "Hassan", "Hedegård", "Hedegaard", "Hemmingsen", "Jeppesen", "Jepsen", "Jespersen", "Jessen", "Johannesen", "Johannsen", "Johansen", "Johansson", "Johnsen", "Schou", "Schrøder", "Schultz", "Simonsen", "Skov"};
+    private static String[] mails = {"gmail.com", "hotmail.com", "yahoo.dk", "live.dk", "outlook.dk"};
+    private static String[] hobbies = {"Fodbold", "Gymnastik", "Fitness", "Svømning", "Golf", "Håndbold", "Badminton", "Ridning"};
+    private static String[] phoneDesc = {"Hjem", "Arbejde", "Mobil"};
+    private static Random random = new Random();
+
+    private static int getNumber(int min, int max) {
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    public static String generateAddresses(int amount) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < amount; i++) {
+            sb.append("INSERT INTO `CA2`.`Address` (`Street`, `additionalinfo`, `Cityinfo_id`) VALUES ('")
+                    .append(addresses[getNumber(0, addresses.length - 1)])
+                    .append(" ")
+                    .append(getNumber(1, 450))
+                    .append("', 'Test', '")
+                    .append(getNumber(1, 1352))
+                    .append("'")
+                    .append(");\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static String generateNames(int amount) {
+        StringBuilder sb = new StringBuilder();
+        String firstName = null;
+        for (int i = 0; i < amount; i++) {
+            firstName = firstNames[getNumber(0, firstNames.length - 1)];
+            sb.append("INSERT INTO `CA2`.`Person` (`email`, `firstname`, `lastname`, `Address_ID`) VALUES ('")
+                    .append(firstName.toLowerCase())
+                    .append("@")
+                    .append(mails[getNumber(0, mails.length - 1)])
+                    .append("', '")
+                    .append(firstName)
+                    .append("', '")
+                    .append(lastNames[getNumber(0, lastNames.length - 1)])
+                    .append("', '")
+                    .append(getNumber(1, amount))
+                    .append("'")
+                    .append(");\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static String generatePhones(int amount) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < amount; i++) {
+            sb.append("INSERT INTO `CA2`.`phone` (`number`, `description`, `Person_ID`) VALUES ('")
+                    .append(getNumber(12345678, 99999999))
+                    .append("', '")
+                    .append(phoneDesc[getNumber(0, phoneDesc.length - 1)])
+                    .append("', '")
+                    .append(getNumber(1, amount))
+                    .append("'")
+                    .append(");\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static String generateHobbies() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < hobbies.length; i++) {
+            sb.append("INSERT INTO `CA2`.`Hobby` (`name`, `description`) VALUES ('")
+                    .append(hobbies[i])
+                    .append("', '")
+                    .append("Test")
+                    .append("');\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static String personHobbies(int amount) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < amount; i++) {
+            sb.append("INSERT INTO `CA2`.`person_hobby` (`hobbies_id`, `Persons_ID`) VALUES ('")
+                    .append(getNumber(1, hobbies.length))
+                    .append("', '")
+                    .append(getNumber(1, amount))
+                    .append("'")
+                    .append(");\n");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateAddresses(5000));
+        System.out.println("");
+        System.out.println(generateNames(5000));
+        System.out.println("");
+        System.out.println(generatePhones(5000));
+        System.out.println("");
+        System.out.println(generateHobbies());
+        System.out.println("");
+        System.out.println(personHobbies(5000));
+    }
+
+}
